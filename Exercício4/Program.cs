32084b124937eed03f94e97564356deec64df6aa
marcela <marcelalopes497@gmail.com>
2022-04-28 23:17:16 -0300

    Exercicio 4

---
+++ Exercício4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 4) Faça um programa em C# e no Visual Studio que receba a data na forma DDMMAAAA e imprima na 
+forma AAAAMMDD e AAMMDD. Note que o dia, o mês e o ano devem ser lidos em variáveis diferentes.
+*/
+
+int ano, mes, dia;
+
+Console.WriteLine("Digite o dia: ");
+dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o mês: ");
+mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o ano: ");
+ano = int.Parse(Console.ReadLine());
+
+DateTime date = new DateTime(ano, mes, dia);
+
+Console.WriteLine(date.ToString("yyyy-mm-dd"));
+Console.WriteLine(date.ToString("yy-mm-dd"));
