ee653021deb59f9c9d091914600777e0a3304b38
marcela <marcelalopes497@gmail.com>
2022-05-25 21:51:00 -0300

    Exercicio5

---
+++ Lista de Exercícios 6/Exercicio5/ItemCenario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    class ItemCenario
+    {
+        private string Descricao;
+        private DateTime DataCriacao;
+        private float Altura;
+
+        public string Descricao1 { get => Descricao; set => Descricao = value; }
+        public DateTime DataCriacao1 { get => DataCriacao; set => DataCriacao = value; }
+        public float Altura1 { get => Altura; set => Altura = value; }
+
+        public ItemCenario(string desc, DateTime dataC, float alt)
+        {
+            this.Descricao1 = desc;
+            this.DataCriacao1 = dataC;
+            this.Altura1 = alt;
+        }
+
+        public string ExibirItem()
+        {
+            return "Descrição: " + this.Descricao1 + "\nData de criação: " + this.DataCriacao1 + "\nAltura: " + Altura1 + "\n----";
+        }
+
+        public void TempoCriacao()
+        {
+            Console.Write("Este item foi criado há ");
+            Console.Write(DateTime.Now - this.DataCriacao1);
+        }
+    }
+}
---
+++ Lista de Exercícios 6/Exercicio5/Program.cs
@@ -0,0 +1,27 @@
+﻿//#5 Crie uma classe para representar um item de cenário, com os atributos privados de:
+//    *descrição(string), 
+//    *data de criação(date - https://docs.microsoft.com/en-us/dotnet/api/system.datetime.date?view=net-6.0) e
+//    *altura(float).
+
+//Crie os métodos públicos necessários para sets e gets, o construtor básico e também um método para imprimir 
+//todos dados de um item de cenário. 
+//Por fim, crie um método para calcular a quanto tempo o item foi criado, além de um programa em C# para testar a classe criada.
+
+using Exercicio5;
+
+string des;
+DateTime dataC;
+float altura;
+
+Console.WriteLine("Digite a descrição do item: ");
+des = Console.ReadLine();
+Console.WriteLine("Digite a data de criação: ");
+dataC = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura do item: ");
+altura = float.Parse(Console.ReadLine());
+Console.Clear();
+
+ItemCenario item = new ItemCenario(des, dataC, altura);
+
+Console.WriteLine(item.ExibirItem());
+item.TempoCriacao();
