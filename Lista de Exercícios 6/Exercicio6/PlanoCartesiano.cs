059ebbbecc29689707b4af2088c9964571b4a03f
marcela <marcelalopes497@gmail.com>
2022-05-26 08:05:56 -0300

    Exercicio6

---
+++ Lista de Exercícios 6/Exercicio6/PlanoCartesiano.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class PlanoCartesiano
+    {
+        private int x;
+        private int y;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public PlanoCartesiano(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Ponto x: {this.X}\nPonto y: {this.Y}\n-----");
+        }
+    }
+}
---
+++ Lista de Exercícios 6/Exercicio6/Program.cs
@@ -0,0 +1,28 @@
+﻿//#6 Crie uma classe para representar um objeto em um Plano Cartesiano. 
+//Todo objeto no plano cartesiano tem como atributos 'x' e 'y'. Dessa forma, crie os getters e setter de 'x' e 'y', 
+//o construtor básico e um método para exibir um objeto (x, y).
+//Por fim, crie um programa em C# que cadastre 'n' objetos em uma lista (List), um método de que exiba
+//os objetos da lista.
+
+using Exercicio6;
+
+List<PlanoCartesiano> listP = new List<PlanoCartesiano>();
+int n,x=0,y=0;
+
+Console.WriteLine("Quantos objetos você deseja cadastrar?");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Clear();
+    Console.WriteLine("Digite o ponto x: ");
+    x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o ponto y: ");
+    y = int.Parse(Console.ReadLine());
+
+    listP.Add(new PlanoCartesiano(x,y));
+}
+for (int i = 0; i < listP.Count; i++)
+{
+    listP[i].ExibirDados();
+}
