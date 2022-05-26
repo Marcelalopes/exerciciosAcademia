42349182df589bbed72d1b51f33e16638816d926
marcela <marcelalopes497@gmail.com>
2022-05-25 21:16:19 -0300

    Exercicio 4

---
+++ Lista de Exercícios 6/Exercicio4/Asteroide.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Asteroide
+    {
+        private int Posix;
+        private int Posiy;
+        private int Tamanho;
+        private int Velocidade;
+        private int Energia;
+
+        public int Posix1 { get => Posix; set => Posix = value; }
+        public int Posiy1 { get => Posiy; set => Posiy = value; }
+        public int Tamanho1 { get => Tamanho; set => Tamanho = value; }
+        public int Velocidade1 { get => Velocidade; set => Velocidade = value; }
+        public int Energia1 { get => Energia; set => Energia = value; }
+
+        public Asteroide()
+        {
+
+        }
+        public Asteroide(int posix, int posiy, int tamanho, int velocidade, int energia)
+        {
+            this.Posix1 = posix;
+            this.Posiy1 = posiy;
+            this.Tamanho1 = tamanho;
+            this.Velocidade1 = velocidade;
+            this.Energia1 = energia;
+        }
+        public Asteroide(int posix, int posiy)
+        {
+            this.Posix1 = posix;
+            this.Posiy1 = posiy;
+        }
+
+        public string ExibirAsteroides()
+        {
+            return "Posição X: "+this.Posix1 + "\nPosição Y: " + this.Posiy1+"\nTamanho do asteroide: "+this.Tamanho1+"\nVelocidade: "+this.Velocidade1+"\nEnergia: "+this.Energia1+"\n--------";
+        }
+    }
+}
---
+++ Lista de Exercícios 6/Exercicio4/Program.cs
@@ -0,0 +1,38 @@
+﻿//#4 Imagine um jogo com asteroides e uma nave de ataque, que deve destruir os asteróides que voam em todas as direções.
+
+//Dessa forma, crie um programa em C# que represente uma lista de asteroides que deveriam ser 'inseridos' no jogo.
+
+//Para isso, faça uma classe que contenha os atributos (privados) posição x, posição y (do asteroide em
+//um plano cartesiano), tamanho do asteroide(1 a 10), velocidade do asteroide(1 a 5) e energia(1 a 5).
+
+//Para esses atributos privados, gerar os métodos de acesso (propriedades getter e setter).
+
+//Além disso, implementar 3 construtores (sobrecarga): um vazio, outro passando todos os parâmetros de um objeto tipo
+//Asteroide, e um terceiro que constrói um asteroide com posição x e posição y.
+
+//O programa principal (main) deve conter a lista de Asteroides, preenchida pelo usuário. Nesse programa principal,
+//construir um método de classe que exiba todos os asteróides da lista.
+
+using Exercicio4;
+
+List<Asteroide> listA = new List<Asteroide>();
+int px = 0, py = 0, tam = 0, velo = 0, ener = 0;
+for (int i = 0; i < 3; i++)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a posição X: ");
+    px = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a posição Y: ");
+    py = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o tamanho do asteroide (1 a 10): ");
+    tam = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a velocidade (1 a 5): ");
+    velo = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a energia (1 a 5): ");
+    ener = int.Parse(Console.ReadLine());
+    listA.Add(new Asteroide(px,py,tam,velo,ener));
+}
+for (int i = 0; i < listA.Count; i++)
+{
+    Console.WriteLine(listA[i].ExibirAsteroides());
+}
